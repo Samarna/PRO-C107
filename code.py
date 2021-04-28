34daa9aeade68f75eb67bc33ac2a854f61abfce3
Samarna <63645732+Samarna@users.noreply.github.com>
2021-04-28 18:54:28 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,12 +2,14 @@ import pandas as pd
 import plotly_express as px 
 
 df = pd.read_csv("data.csv")
-student_df = df.loc[df["student_id"]== "TRL_xyz"]
+mean = df.groupby(["student_id", "level"], as_index = False)["attempt"].mean()
 
 # showing bar graph using plotly express
 fig = px.scatter(
-    x = student_df.groupby("level")["attempt"].mean(),
-    y = ["Level 1","Level 2","Level 3","Level 4"]
+    mean,
+    x = "student_id",
+    y = "level",
+    size = "attempt",
+    color = "attempt"
 )
 fig.show()
-
